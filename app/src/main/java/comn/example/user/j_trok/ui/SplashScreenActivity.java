d065fe04145e1f574a7530bc7671848e2c50011f
larrytech7 <larryakah@gmail.com>
2017-05-17 22:54:25 +0100

    Added some cool touch to the splash screen

--- app/src/main/java/comn/example/user/j_trok/ui/SplashScreenActivity.java
+++ app/src/main/java/comn/example/user/j_trok/ui/SplashScreenActivity.java
@@ -2,36 +2,41 @@ package comn.example.user.j_trok.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 import comn.example.user.j_trok.R;
 
 public class SplashScreenActivity extends AppCompatActivity {
+
+    private TextView sloganTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
-        Thread timerThread = new Thread(){
-            public void run(){
-                try{
-                    sleep(3000);
-                }catch(InterruptedException e){
-                    e.printStackTrace();
-                }finally{
-                    Intent intent = new Intent(SplashScreenActivity.this,IntroSliderActivity.class);
-                    startActivity(intent);
-                }
+        sloganTextView = (TextView) findViewById(R.id.textViewSlogan);
+
+        new CountDownTimer(8000, 1000){
+
+            int i = 7;
+
+            @Override
+            public void onTick(long l) {
+                //change the text in the splash message
+
+                sloganTextView.setText(getString(R.string.slogan, i--));
             }
-        };
-        timerThread.start();
+
+            @Override
+            public void onFinish() {
+                Intent intent = new Intent(SplashScreenActivity.this,IntroSliderActivity.class);
+                startActivity(intent);
+                SplashScreenActivity.this.finish();
+            }
+        }.start();
     }
 
-    @Override
-    protected void onPause() {
-        // TODO Auto-generated method stub
-        super.onPause();
-        finish();
-    }
 }
