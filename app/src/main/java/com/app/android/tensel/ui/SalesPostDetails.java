11d1a934fb4ecc2bc2f8740f88deb533a9aea21d
Nguedia_Adele <nguimatiobest@gmail.com>
2017-07-12 12:12:26 +0100

    sales post details added

---
+++ app/src/main/java/com/app/android/tensel/ui/SalesPostDetails.java
@@ -0,0 +1,15 @@
+package com.app.android.tensel.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.app.android.tensel.R;
+
+public class SalesPostDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sales_post_details);
+    }
+}
